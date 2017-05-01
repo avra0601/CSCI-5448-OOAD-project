d51bed31ec788f67ac4559d8ba0c73476507cc69
Monikatak <monikatak425@gmail.com>
2017-05-01 11:09:55 -0600

    Add files via upload

---
+++ Project Code/SellerDashboardView.java
@@ -0,0 +1,65 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class SellerDashboardView{
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	 
+
+	/**
+	 * Create the application.
+	 */
+	public SellerDashboardView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1018, 723);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setVisible(true);
+		JButton btnNewButton = new JButton("Message Inbox");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			MessageBoxView mv=new MessageBoxView();
+			}
+		});
+		btnNewButton.setBounds(484, 78, 164, 29);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Logout");
+		btnNewButton_1.setBounds(748, 78, 115, 29);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Add a New Item");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				ItemView iv=new ItemView();
+			}
+		});
+		btnNewButton_2.setBounds(360, 223, 224, 29);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("View Items Added");
+		btnNewButton_3.setBounds(360, 306, 224, 29);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Update Profile");
+		btnNewButton_4.setBounds(360, 416, 224, 29);
+		frame.getContentPane().add(btnNewButton_4);
+	}
+
+}
