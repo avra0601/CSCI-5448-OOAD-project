7d670b1a513c396a016956e2df08600f7292b7a9
Monikatak <monikatak425@gmail.com>
2017-05-02 08:20:56 -0600

    Add files via upload
    Observe Subject Interface

---
+++ Project Code/Observer.java
@@ -0,0 +1,5 @@
+
+public interface Observer {
+
+	public void update(int status , String itemid);
+}
---
+++ Project Code/Subject.java
@@ -0,0 +1,9 @@
+
+public interface Subject {
+
+	public void registerObserver(Observer observer);
+	
+	
+	public void notifyObservers();
+	
+}
